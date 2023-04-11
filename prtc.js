58ed94e2339259cfd950091e1701737c065b6e1e
qqUnknowUserqq <45928848+qqbbpsy@users.noreply.github.com>
2023-04-11 14:09:17 +0300

    Update prtc.js

--- prtc.js
+++ prtc.js
@@ -1,21 +1,21 @@
 $(document).keydown(function (event) {
     if (event.keyCode == 123) { // Prevent F12
-        alert(&#39;Content is protected\nYou cannot view the Dev Tools.&#39;);
+        alert('Content is protected\nYou cannot view the Dev Tools.');
 		return false;
-    } else if (event.ctrlKey &amp;&amp; event.shiftKey &amp;&amp; event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-        alert(&#39;Content is protected\nYou cannot view the Dev Tools.&#39;);
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        alert('Content is protected\nYou cannot view the Dev Tools.');
 		return false;
     }
 });
 
-document.addEventListener(&quot;contextmenu&quot;, function(e) {
-  if (!e.target.matches(&quot;img&quot;)) {
+document.addEventListener("contextmenu", function(e) {
+  if (!e.target.matches("img")) {
     e.preventDefault();
   }
 });
 
 document.onkeydown = function(e) {
-        if (e.ctrlKey &amp;&amp;
+        if (e.ctrlKey &&
             (e.keyCode === 85 )) {
             return false;
         }
